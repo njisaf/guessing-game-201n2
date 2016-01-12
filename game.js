e2415ec618191814582d1cd261ebfe961a4ff6b6
Daniel Wade <dcwade87@gmail.com>
2016-01-12 13:36:18 -0800

    Draft before more while loops

--- game.js
+++ game.js
@@ -1,15 +1,42 @@
+/* Definiing variable */
             var myAge = [28, "twenty eight", "twenty-eight", "twentyeight"];
             var correctGuess = 0
+            var myNumber = 8
+            var myNumberCount = 0
 
-            alert("Welcome to this REALLY cool page!!!  Let's play a game shall we?");
-            function getSmart(){
-            var guessAge = prompt ("To view this page you must first guess my age.\nHow old am I?");
+/*Intro text*/
+
+            alert("Welcome to this REALLY cool page!!!  Let's play some games shall we?");
+            alert("To view this page you must first guess my age.");
                 console.log("User has entered the number" + " " + guessAge);
 
-    if (guessAge == myAge[0]) {
+/*Age guess start*/
+
+    while (myAge) {
+            var guessAge = prompt ("How old am I?")
+
+    if (guessAge == myAge[0] || guessAge.toLowerCase() == myAge[1] || guessAge.toLowerCase() == myAge[2] || guessAge.toLowerCase() == myAge[3] ) {
             correctGuess += 1
-            function colorTest(){
-            alert("Impressive.  Yet ye who would see this page must first answer me these questions three...");
+            myAge = false;
+            alert("Alright hot shot, good job.\nNow I'm thinking of a number between 1 and 10.  Can you guess it?");
+
+/*Number guess start*/
+
+    while (myNumber){
+            var guessNumber = prompt("What is my number?");
+
+    if (guessNumber != 8) {
+            alert("Wrong!  Try again.");
+            myNumberCount += 1
+
+    } else {
+            myNumberCount += 1
+            alert("Impressive!  That only took you" + " " + myNumberCount + " " + "tries.\nBut to see this page you must first answer these three questions...");
+            myNumber = false; } }
+
+/*Number guess end*/
+
+/*Name start*/
             function spellName(){
             var userName = prompt("What...Is your name?");
                 console.log("User's name is" + " " + userName);
@@ -23,7 +50,6 @@
             alert("I highly doubt your name is a number... Spell out your name!")
                 console.log("User entered an integer as their name")
                   spellName();
-
 
    } else {
               var quest = prompt("What...Is your quest?");
@@ -44,33 +70,26 @@
               alert("Okay" + " " + userName + ", " + "that's nice, but" + " " + favColor.toLowerCase() + " " + "is a terrible color so BYE FELICIA!");
                   window.close(); } }
                     }spellName();
-                    }colorTest();
 
-
-
-  } else if (guessAge.toLowerCase() == myAge[1] || guessAge.toLowerCase() == myAge[2] || guessAge.toLowerCase() == myAge[3] ) {
-            correctGuess += 1
-                    colorTest();
+/*Age guess continued*/
 
   } else if (guessAge > 32) {
             alert("Your guess is way too high!");
-            function wrongAge(){
-                  console.log("User guessed incorrectly.  Restarting code.");
-                    getSmart();
-                    }wrongAge();
+            console.log("User guessed way too high");
 
   } else if (guessAge >= 29 && guessAge <= 32) {
             alert("Your guess is too high but very close.  Try a lower number!")
-                    wrongAge();
+            console.log("User guessed too high");
 
   } else if (guessAge < 23) {
             alert("Your guess is way too low!");
-                    wrongAge();
+            console.log("User guessed way too low");
 
   } else if (guessAge >= 23 && guessAge <= 27) {
             alert("Your guess is too low but very close.  Try a higher number!");
-                    wrongAge();
+            console.log("User guessed too low");
 
-  } else {  alert("That answer is wrong.  Try guessing with an integer!")
-                    wrongAge();}
-                    }getSmart();
+  } else {  alert("That answer is downright wrong.  Try guessing with an integer!")
+            console.log("User guess with a string"); } }
+
+/*Age guess end*/
