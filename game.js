98fca94f334691b316758f5e07b3fb45b5f3aa7e
Daniel Wade <dcwade87@gmail.com>
2016-01-12 17:09:27 -0800

    Added code for total questions guessed correctly, hidden point, etc

--- game.js
+++ game.js
@@ -1,42 +1,35 @@
-/* Definiing variable */
+
             var myAge = [28, "twenty eight", "twenty-eight", "twentyeight"];
             var correctGuess = 0
             var myNumber = 8
             var myNumberCount = 0
 
-/*Intro text*/
-
             alert("Welcome to this REALLY cool page!!!  Let's play some games shall we?");
             alert("To view this page you must first guess my age.");
-                console.log("User has entered the number" + " " + guessAge);
-
-/*Age guess start*/
 
     while (myAge) {
             var guessAge = prompt ("How old am I?")
-
+                console.log("User has entered the number" + " " + guessAge);
     if (guessAge == myAge[0] || guessAge.toLowerCase() == myAge[1] || guessAge.toLowerCase() == myAge[2] || guessAge.toLowerCase() == myAge[3] ) {
             correctGuess += 1
             myAge = false;
             alert("Alright hot shot, good job.\nNow I'm thinking of a number between 1 and 10.  Can you guess it?");
-
-/*Number guess start*/
 
     while (myNumber){
             var guessNumber = prompt("What is my number?");
 
     if (guessNumber != 8) {
             alert("Wrong!  Try again.");
+            console.log("User guessed incorrectly")
             myNumberCount += 1
 
     } else {
+            correctGuess += 1
             myNumberCount += 1
             alert("Impressive!  That only took you" + " " + myNumberCount + " " + "tries.\nBut to see this page you must first answer these three questions...");
+            console.log("User guessed the number 8 after" + " " + myNumberCount + " " + "tries.")
             myNumber = false; } }
 
-/*Number guess end*/
-
-/*Name start*/
             function spellName(){
             var userName = prompt("What...Is your name?");
                 console.log("User's name is" + " " + userName);
@@ -52,26 +45,35 @@
                   spellName();
 
    } else {
+              correctGuess += 1
               var quest = prompt("What...Is your quest?");
-                  console.log(userName + "'s ultimate goal is" + " " + quest.toLowerCase());
+                  console.log(userName + "'s ultimate goal is" + " " + quest.toLowerCase()); }
 
+      if (quest.toLowerCase() === "holy grail" || quest.toLowerCase() === "holygrail" || quest.toLowerCase() === "to seek the holy grail" || quest.toLowerCase() === "to obtain the holy grail") {
+              correctGuess += 1
+              alert("You sure do know your Monty Python references!  You got the bonus point!");
+              function monthPython(){
               var favColor = prompt("What...Is your favorite color?");
                   console.log(userName + "'s favorite color is" + " " + favColor.toLowerCase());
 
-   if (favColor.toLowerCase() === "purple") {
+              if (favColor.toLowerCase() === "purple") {
+                  correctGuess += 1
                   document.title = (quest.toUpperCase()+("!!!"));
-              alert("Excellent work" + " " + userName + ", " + "you have chosen wisely!  Now enjoy this pleasantly purple page!");
+              alert("Excellent work" + " " + userName + ", " + "you have chosen wisely!\nYou have guessed" + " " + correctGuess + " " + "out of 5 questions right!\nNow enjoy this pleasantly purple page!");
                   console.log(userName + " " + "has amazing taste and will now be shown this awesome page");
                   document.getElementById('hiddenContent').style.display= "block";
                   document.getElementById("name").innerHTML = ("CONGRATULATIONS" + " " + userName.toUpperCase() + "!!!");
 
-  } else {
+              } else {
                   console.log(userName + " " + "has poor taste and will now be ejected from the website")
               alert("Okay" + " " + userName + ", " + "that's nice, but" + " " + favColor.toLowerCase() + " " + "is a terrible color so BYE FELICIA!");
-                  window.close(); } }
-                    }spellName();
+                  window.close(); }
+                    }monthPython();
 
-/*Age guess continued*/
+   } else {
+                    monthPython();
+            }
+                    }spellName();
 
   } else if (guessAge > 32) {
             alert("Your guess is way too high!");
@@ -91,5 +93,3 @@
 
   } else {  alert("That answer is downright wrong.  Try guessing with an integer!")
             console.log("User guess with a string"); } }
-
-/*Age guess end*/
